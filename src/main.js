4d87d99ee71abdbd33c45179df7a4e74b06665a2
lastofyears21 <lastofyears21@gmail.com>
2024-05-31 16:47:19 +0900

    24/05/31 정벼리 설정 추가

--- src/main.js
+++ src/main.js
@@ -4,7 +4,7 @@ import router from '@/router';
 import store from '@/store';
 import axios from 'axios';
 import '@/assets/style.css'; 
-
+import './assets/tailwind.css'
 // 실제 데이터를 json으로 주고받기 위해 무언가 요청할 때 사용할 수 있도록 설정
 axios.defaults.headers.post["Content-Type"] = "application/x-www-form-urlencoded";
 
