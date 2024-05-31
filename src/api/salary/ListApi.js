a7f47a4bbfbe304dc24b545f7740933bd5f620e2
SeokJune <op2se1@gmail.com>
2024-05-31 17:21:33 +0900

    20240531.

---
+++ src/api/salary/ListApi.js
@@ -0,0 +1,14 @@
+import apiClient from "@/api/axios";
+
+// 급여 항목, 사원 정보, 급여 총계
+export const selectAllLists = async (salaryDate) => {
+  try {
+    const response = await apiClient.get("/salary/lists", {
+      params: { salaryDate },
+    });
+    return response.data;
+  } catch (error) {
+    console.error("Error fetching lists:", error);
+    throw error;
+  }
+};
