b99d492c764424ac746d9378268aa9668458c4f2
lastofyears21 <lastofyears21@gmail.com>
2024-06-03 16:07:02 +0900

    24/06/03 정벼리 prettier 설정 추가

--- src/views/sales/SalesPartner.vue
+++ src/views/sales/SalesPartner.vue
@@ -1,34 +1,38 @@
 <template>
   <div>
     <h1>협력사 관리</h1>
-    <button @click="currentComponent = 'HotelDashboard'">Show Hotel Dashboard</button>
-    <button @click="currentComponent = 'AgencyDashboard'">Show Agency Dashboard</button>
-    <button @click="currentComponent = 'FlightDashboard'">Show Flight Dashboard</button>
+    <button @click="currentComponent = 'HotelDashboard'">
+      Show Hotel Dashboard
+    </button>
+    <button @click="currentComponent = 'AgencyDashboard'">
+      Show Agency Dashboard
+    </button>
+    <button @click="currentComponent = 'FlightDashboard'">
+      Show Flight Dashboard
+    </button>
 
     <component :is="currentComponent"></component>
   </div>
 </template>
 
 <script>
-import HotelDashboard from '@/components/sales/HotelDashboard.vue'; 
-import AgencyDashboard from '@/components/sales/AgencyDashboard.vue'; 
-import FlightDashboard from '@/components/sales/FlightDashboard.vue'; 
-
+import HotelDashboard from "@/components/sales/HotelDashboard.vue";
+import AgencyDashboard from "@/components/sales/AgencyDashboard.vue";
+import FlightDashboard from "@/components/sales/FlightDashboard.vue";
 
 export default {
-  name: 'SalesPartner',
+  name: "SalesPartner",
   components: {
     HotelDashboard,
     AgencyDashboard,
-    FlightDashboard
+    FlightDashboard,
   },
   data() {
     return {
-      currentComponent: 'HotelDashboard'
+      currentComponent: "HotelDashboard",
     };
-  }
+  },
 };
 </script>
 
-<style>
-</style>
+<style></style>
