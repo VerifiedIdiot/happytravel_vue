a0db2024b46fbc3e2e4a93e9584644e44e6fd5ad
moonjiye <mjy6088@gmail.com>
2024-06-03 22:14:34 +0900

    24/06/03 뭉 호텔상품CRUD

--- src/components/sales/HotelDashboard.vue
+++ src/components/sales/HotelDashboard.vue
@@ -2,7 +2,7 @@
   <div>
     <h1>Hotel Dashboard</h1>
     <div>
-      <button>신규등록</button>
+      <button @click="openModal()">신규등록</button>
     </div>
     <div>
       <table>
@@ -19,7 +19,7 @@
           </tr>
         </thead>
         <tbody>
-          <tr v-for="hotel in hotels" :key="hotel.hotel_code">
+          <tr v-for="hotel in isUsedHotel" :key="hotel.hotel_code" >
             <td>{{ hotel.hotel_name }}</td>
             <td>{{ hotel.phone }}</td>
             <td>{{ hotel.country }}</td>
@@ -35,7 +35,7 @@
     <div v-if="showModal" class="modal">
       <div class="modal-content">
         <span class="close" @click="closeModal">&times;</span>
-        <h2>Hotel 수정</h2>
+        <h2>{{ newHotel ? 'Hotel 등록' : 'Hotel 수정' }}</h2>
         <form @submit.prevent="saveHotel">
           <div>
             <label for="hotel_name">Hotel Name:</label>
@@ -69,7 +69,7 @@
             </select>
           </div>
           <div>
-            <button type="submit">저장</button>
+            <button type="submit">{{ newHotel ? '등록' : '저장' }}</button>
           </div>
         </form>
       </div>
@@ -78,8 +78,8 @@
 </template>
 
 <script>
-import { ref, onMounted } from 'vue';
-import { getHotelList, updateHotel } from '@/api/sales/HotelApi';
+import { ref, computed, onMounted } from 'vue';
+import { getHotelList,getLastHotelCode,insertHotel, updateHotel } from '@/api/sales/HotelApi';
 
 export default {
   name: 'HotelDashboard',
@@ -87,7 +87,7 @@ export default {
     const hotels = ref([]);
     const showModal = ref(false);
     const currentHotel = ref({});
-    const empId = 'EMP30002';
+    const newHotel = ref(true);
 
     const formatPrice = (price) => {
       return new Intl.NumberFormat('ko-KR', { style: 'currency', currency: 'KRW' }).format(price);
@@ -95,6 +95,7 @@ export default {
 
     const fetchHotels = async () => {
       try {
+        
         hotels.value = await getHotelList();
         console.log('Fetched flights:', hotels.value);
       } catch (error) {
@@ -105,6 +106,7 @@ export default {
     const openModal = (hotel) => {
       if (hotel) {
         currentHotel.value = { ...hotel };
+        newHotel.value = false;
       } else {
         currentHotel.value = {
           hotel_name: '',
@@ -115,6 +117,7 @@ export default {
           price: '',
           is_used: 'Y',
         };
+        newHotel.value = true;
       }
       showModal.value = true;
     };
@@ -125,11 +128,33 @@ export default {
 
     const saveHotel = async () => {
       try {
-        currentHotel.value.empId = empId;
-        console.log('Updating hotel with data:', currentHotel.value);
+        const empId = sessionStorage.getItem('empId') || 'EMP30002';
+        // currentHotel.value.empId = empId;
+        console.log('Updating hotel with data:', currentHotel.value, empId);
         const hotelCode = currentHotel.value.hotel_code;
-        await updateHotel(hotelCode, currentHotel.value);
 
+        if(newHotel.value) {
+          const lastHotelCode = await getLastHotelCode();
+          let newHotelCode;
+          if (lastHotelCode) {
+            const numericPart = parseInt(lastHotelCode.replace('H', ''), 10);
+            const newNumericPart = numericPart + 1;
+            newHotelCode = `H${String(newNumericPart).padStart(3, '0')}`;
+          } else {
+            newHotelCode = 'H001';
+          }
+          currentHotel.value.hotel_code = newHotelCode;
+          const newHotel = await insertHotel(currentHotel.value);
+          hotels.value.push(newHotel);
+        } else {
+          const hotelCode = currentHotel.value.hotel_code;
+          await updateHotel(hotelCode, currentHotel.value);
+          const index = hotels.value.findIndex(h => h.hotel_code === hotelCode);
+          if (index !== -1) {
+            hotels.value[index] = { ...currentHotel.value };
+          }
+        }
+        
         const index = hotels.value.findIndex(h => h.hotel_code === hotelCode);
         if (index !== -1) {
           hotels.value[index] = { ...currentHotel.value };
@@ -143,6 +168,10 @@ export default {
       }
     };
 
+    const isUsedHotel = computed(() => {
+      return hotels.value.filter(hotel => hotel.is_used === 'Y');
+    });
+
     onMounted(fetchHotels);
 
     return {
@@ -153,6 +182,8 @@ export default {
       openModal,
       closeModal,
       saveHotel,
+      newHotel,
+      isUsedHotel,
     };
   },
 };
