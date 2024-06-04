4ce5a6f10295c2b3f2402f4648bb07a2666eef28
lastofyears21 <lastofyears21@gmail.com>
2024-06-04 10:01:52 +0900

    24/05/04 정벼리 여행상품 상세 정보 모달찰  css 레이아웃  완료

--- src/components/sales/package/PackageDetail.vue
+++ src/components/sales/package/PackageDetail.vue
@@ -1,77 +1,146 @@
 <template>
   <div class="modal-overlay" @keydown.esc="handleClose">
     <div class="modal-content">
-      <h1>여행상품 상세</h1>
+      <div class="modal-header">
+        <div class="modal-header-item">
+          <img src="@/assets/icons/flight.png" />
+        </div>
+        <h1>여행상품 상세</h1>
+      </div>
+
       <form @submit.prevent="submitForm">
-        <div>
-          <label for="packageName">Package Name:</label>
-          <span v-if="!isEditing">{{ packageDetail.package_name }}</span>
-          <input v-else type="text" id="packageName" v-model="packageDetail.package_name" />
+        <div class="form-upper">
+          <div>
+            <label for="packageName"><legend>상품 이름</legend></label>
+            <span v-if="!isEditing">{{ packageDetail.package_name }}</span>
+            <input
+              v-else
+              type="text"
+              id="packageName"
+              v-model="packageDetail.package_name" />
+
+            <label for="country"><legend>국가</legend></label>
+            <span v-if="!isEditing">{{ packageDetail.country }}</span>
+            <input
+              v-else
+              type="text"
+              id="country"
+              v-model="packageDetail.country" />
+          </div>
+          <div>
+            <label for="startDate"><legend>여행시작일</legend></label>
+            <span v-if="!isEditing">{{ packageDetail.start_date }}</span>
+            <input
+              v-else
+              type="date"
+              id="startDate"
+              v-model="packageDetail.start_date" />
+            <label for="endDate"><legend>여행종료일</legend></label>
+            <span v-if="!isEditing">{{ packageDetail.end_date }}</span>
+            <input
+              v-else
+              type="date"
+              id="endDate"
+              v-model="packageDetail.end_date" />
+          </div>
+          <div>
+            <label for="saleStartDate"><legend>판매시작일</legend></label>
+            <span v-if="!isEditing">{{ packageDetail.sale_start_date }}</span>
+            <input
+              v-else
+              type="date"
+              id="saleStartDate"
+              v-model="packageDetail.sale_start_date" />
+            <label for="saleEndDate"><legend>판매종료일</legend></label>
+            <span v-if="!isEditing">{{ packageDetail.sale_end_date }}</span>
+            <input
+              v-else
+              type="date"
+              id="saleEndDate"
+              v-model="packageDetail.sale_end_date" />
+          </div>
         </div>
-        <div>
-          <label for="country">Country:</label>
-          <span v-if="!isEditing">{{ packageDetail.country }}</span>
-          <input v-else type="text" id="country" v-model="packageDetail.country" />
+        <div class="form-middle">
+          <div>
+            <label for="agencyCode"><legend>현지 여행사 정보</legend></label>
+            <div class="partner-info">
+            <span v-if="!isEditing">{{ packageDetail.agency_code }}</span>
+            <input
+              v-else
+              type="text"
+              id="agencyCode"
+              v-model="packageDetail.agency_code" />
+            </div>
+          </div>
+          <div>
+            <label for="flightCode"><legend>항공권 정보</legend></label>
+            <div class="partner-info">
+            <span v-if="!isEditing">{{ packageDetail.flight_code }}</span>
+            <input
+              v-else
+              type="text"
+              id="flightCode"
+              v-model="packageDetail.flight_code" />
+            </div>
+          </div>
+          <div>
+            <label for="hotelCode"><legend>호텔 정보</legend></label>
+            <div class="partner-info">
+            <span v-if="!isEditing">{{ packageDetail.hotel_code }}</span>
+            <input
+              v-else
+              type="text"
+              id="hotelCode"
+              v-model="packageDetail.hotel_code" />
+            </div>
+          </div>
         </div>
-        <div>
-          <label for="startDate">Start Date:</label>
-          <span v-if="!isEditing">{{ packageDetail.start_date }}</span>
-          <input v-else type="date" id="startDate" v-model="packageDetail.start_date" />
+        <div class="form-under">
+          <div>
+            <label for="totalPrice"><legend>원가</legend></label>
+            <span>{{ packageDetail.total_price }}</span>
+          </div>
+          <div>
+            <label for="salePrice"><legend>판매가</legend></label>
+            <span>{{ packageDetail.sale_price }}</span>
+          </div>
+          <div>
+            <label for="saleAmount"><legend>판매량</legend></label>
+            <span v-if="!isEditing">{{ packageDetail.sale_amount }}</span>
+            <input
+              v-else
+              type="number"
+              id="saleAmount"
+              v-model="packageDetail.sale_amount" />
+          </div>
+          <div>
+            <label for="assignCode"><legend>승인상태</legend></label>
+            <span v-if="!isEditing">{{ packageDetail.assign_code }}</span>
+            <input
+              v-else
+              type="text"
+              id="assignCode"
+              v-model="packageDetail.assign_code" />
+          </div>
         </div>
-        <div>
-          <label for="endDate">End Date:</label>
-          <span v-if="!isEditing">{{ packageDetail.end_date }}</span>
-          <input v-else type="date" id="endDate" v-model="packageDetail.end_date" />
-        </div>
-        <div>
-          <label for="saleStartDate">Sale Start Date:</label>
-          <span v-if="!isEditing">{{ packageDetail.sale_start_date }}</span>
-          <input v-else type="date" id="saleStartDate" v-model="packageDetail.sale_start_date" />
-        </div>
-        <div>
-          <label for="saleEndDate">Sale End Date:</label>
-          <span v-if="!isEditing">{{ packageDetail.sale_end_date }}</span>
-          <input v-else type="date" id="saleEndDate" v-model="packageDetail.sale_end_date" />
-        </div>
-        <div>
-          <label for="assignCode">Assign Code:</label>
-          <span v-if="!isEditing">{{ packageDetail.assign_code }}</span>
-          <input v-else type="text" id="assignCode" v-model="packageDetail.assign_code" />
-        </div>
-        <div>
-          <label for="agencyCode">Agency Code:</label>
-          <span v-if="!isEditing">{{ packageDetail.agency_code }}</span>
-          <input v-else type="text" id="agencyCode" v-model="packageDetail.agency_code" />
-        </div>
-        <div>
-          <label for="flightCode">Flight Code:</label>
-          <span v-if="!isEditing">{{ packageDetail.flight_code }}</span>
-          <input v-else type="text" id="flightCode" v-model="packageDetail.flight_code" />
-        </div>
-        <div>
-          <label for="hotelCode">Hotel Code:</label>
-          <span v-if="!isEditing">{{ packageDetail.hotel_code }}</span>
-          <input v-else type="text" id="hotelCode" v-model="packageDetail.hotel_code" />
-        </div>
-        <div>
-          <label for="saleAmount">Sale Amount:</label>
-          <span v-if="!isEditing">{{ packageDetail.sale_amount }}</span>
-          <input v-else type="number" id="saleAmount" v-model="packageDetail.sale_amount" />
-        </div>
-        <div>
-          <label for="salePrice">Sale Price:</label>
-          <span>{{ packageDetail.sale_price }}</span>
-        </div>
-        <div>
-          <label for="totalPrice">Total Price:</label>
-          <span>{{ packageDetail.total_price }}</span>
-        </div>
-        <button type="button" class="btn-update" v-if="!isEditing" @click="isEditing = true">수정하기</button>
-        <button type="submit" class="btn-update" v-if="isEditing">저장</button>
-        <button type="button" class="btn-delete" @click="handleDelete">삭제하기</button>
-        <button type="button" class="btn-close" @click="handleClose">닫기</button>
+        
       </form>
-      
+      <div class="button-container">
+        <button
+          type="button"
+          class="btn-update"
+          v-if="!isEditing"
+          @click="isEditing = true">
+          수정하기
+        </button>
+        <button type="submit" class="btn-update" v-if="isEditing">저장</button>
+        <!-- <button type="button" class="btn-delete" @click="handleDelete">
+          삭제하기
+        </button> -->
+        <button type="button" class="btn-close" @click="handleClose">
+          닫기
+        </button>
+      </div>
     </div>
   </div>
 </template>
@@ -92,7 +161,10 @@ export default {
     onMounted(async () => {
       if (packageCode && packageCode.value) {
         try {
-          const data = await getPackage({ packageCode: packageCode.value, empId });
+          const data = await getPackage({
+            packageCode: packageCode.value,
+            empId,
+          });
           console.log(data);
           packageDetail.value = data;
           // 권한 확인 로직
@@ -138,9 +210,9 @@ export default {
       hasEditPermission,
       submitForm,
       handleDelete,
-      handleClose
+      handleClose,
     };
-  }
+  },
 };
 </script>
 
