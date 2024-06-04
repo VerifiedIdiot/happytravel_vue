2cdd85634e5703f1e9ded54e6ce1cc6c3ffba6a2
mshshshm <hysung2588@gmail.com>
2024-06-04 21:34:09 +0900

    Fix Hrmain

--- src/views/hr/HRMain.vue
+++ src/views/hr/HRMain.vue
@@ -1,17 +1,14 @@
 <template>
   <div>
     <h1>HR Main Page</h1>
-    <AttendanceCalender />
   </div>
 </template>
 
 <script>
-import AttendanceCalender from "@/components/attendances/AttendanceCalender.vue";
 
 export default {
   name: "HRMain",
   components: {
-    AttendanceCalender,
   },
 };
 </script>
