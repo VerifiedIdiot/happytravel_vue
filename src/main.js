e5e88e4609e5d4adef94d0040e759a2c7c5ba9ff
SeokJune <46333532+SeokJune@users.noreply.github.com>
2024-06-04 18:47:16 +0900

    Update main.js

--- src/main.js
+++ src/main.js
@@ -4,9 +4,8 @@ import router from '@/router';
 import store from '@/store';
 import axios from 'axios';
 import '@/assets/style.css';
-// ================================================== 20240603 - S ==================================================
+
 import formatNumber from '@/api/FormatNumber';
-// ================================================== 20240603 - E ==================================================
 
 import '@/assets/styles/style.css';
 // 실제 데이터를 json으로 주고받기 위해 무언가 요청할 때 사용할 수 있도록 설정
@@ -22,7 +21,5 @@ app.use(store)
 app.config.globalProperties.axios = axios;
 app.config.productionTip = false;
 
-// ================================================== 20240603 - S ==================================================
 app.config.globalProperties.formatNumber = formatNumber;
-// ================================================== 20240603 - E ==================================================
 app.config.productionTip = false;
