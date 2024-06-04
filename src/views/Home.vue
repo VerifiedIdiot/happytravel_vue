370ab533658be4e4606de327317c772524a47498
taehyunkim44 <eiiyy12@nate.com>
2024-06-04 10:38:00 +0900

    변경된 설정 원위치

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,6 @@
 <template>
   <div>
     <h1>Home Page</h1>
-    <button @click="goToMyPage">Go to MyPage</button>
     <nav></nav>
   </div>
 </template>
@@ -10,27 +9,7 @@
 export default {
   // eslint-disable-next-line vue/multi-word-component-names
   name: "Home",
-  methods: {
-    goToMyPage() {
-      this.$router.push({ name: "MyPage" });
-    },
-  },
 };
 </script>
 
-<style scoped>
-.home {
-  text-align: center;
-}
-button {
-  padding: 10px 20px;
-  background-color: #007bff;
-  color: white;
-  border: none;
-  border-radius: 4px;
-  cursor: pointer;
-}
-button:hover {
-  background-color: #0056b3;
-}
-</style>
+<style></style>
--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,7 @@
 const { defineConfig } = require("@vue/cli-service");
 const webpack = require("webpack");
 
-const TARGET_URL = "http://localhost:8080";
+const TARGET_URL = "http://localhost:80";
 
 module.exports = defineConfig({
   transpileDependencies: true,
