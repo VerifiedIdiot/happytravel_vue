bfb37cfce8b2acc97d0078036e886e1802863959
moonjiye <mjy6088@gmail.com>
2024-06-11 19:39:09 +0900

    24/06/11 최신화

---
+++ src/components/sales/hotel/HotelModal.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="modal-overlay" @keydown.esc="handleClose">
+    <div class="modal-content">
+      <div class="modal-header">
+        <div class="modal-header-item">
+          <img
+            src="@/assets/icons/hotel2.png"
+            alt="hotel image"
+            width="100"
+            height="100"
+            loading="lazy"
+          />
+        </div>
+        <h1>{{ title }}</h1>
+      </div>
+      <slot></slot>
+    </div>
+  </div>
+</template>
+
+<script>
+import { inject } from "vue";
+
+export default {
+  name: "HotelModal",
+  props: {
+    title: {
+      type: String,
+      default: "호텔상품 상세",
+    },
+    headerIcon: {
+      type: String,
+      default: "@/assets/icons/hotel2.png",
+    },
+  },
+  emits: ['close' ],
+  setup(_, { emit }) {
+    const isEditing = inject('isEditing');
+
+    const handleClose = () => {
+      isEditing.value = false;
+      emit('close');
+    };
+
+    return {
+      handleClose,
+    };
+  },
+};
+</script>
+
+<style src="./HotelDashboard.css"></style>
