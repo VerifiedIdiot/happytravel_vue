b6700c8f1e30b220b5d5341811baed53ffee59ad
moonjiye <mjy6088@gmail.com>
2024-06-13 09:22:08 +0900

    24/06/13 머지오류로 리셋후 재푸시

--- src/components/sales/hotel/HotelDetail.vue
+++ src/components/sales/hotel/HotelDetail.vue
@@ -4,23 +4,43 @@
       <div class="form-box">
         <div class="form-item">
           <label for="hotel_name">호텔명</label>
-          <input type="text" v-model="hotelDetail.hotel_name" required />
+          <span v-if="!hotelState.isEditing">{{
+            hotelState.hotelDetail.hotel_name
+          }}</span>
+          <input
+            type="text"
+            v-else
+            v-model="hotelState.hotelDetail.hotel_name"
+            required
+          />
         </div>
         <div class="form-item">
           <label for="phone">전화번호</label>
-          <input type="text" v-model="hotelDetail.phone" required />
+          <span v-if="!hotelState.isEditing">{{
+            hotelState.hotelDetail.phone
+          }}</span>
+          <input
+            type="text"
+            v-else
+            v-model="hotelState.hotelDetail.phone"
+            required
+          />
         </div>
         <div class="form-item">
           <label for="country">국가</label>
+          <span v-if="!hotelState.isEditing">{{
+            hotelState.hotelDetail.country
+          }}</span>
           <select
+            class="select-country"
+            v-else
             id="country"
-            v-model="hotelDetail.country_code"
-            @change="updateCountryName"
+            v-model="hotelState.hotelDetail.country"
           >
             <option
-              v-for="country in countries"
+              v-for="country in hotelState.countries"
               :key="country.country_code"
-              :value="country.country_code"
+              :value="country.korean_name"
             >
               {{ country.korean_name }}
             </option>
@@ -28,65 +48,51 @@
         </div>
         <div class="form-item">
           <label for="address">주소</label>
-          <input type="text" v-model="hotelDetail.address" required />
+          <span v-if="!hotelState.isEditing">{{
+            hotelState.hotelDetail.address
+          }}</span>
+          <input
+            type="text"
+            v-else
+            v-model="hotelState.hotelDetail.address"
+            required
+          />
         </div>
         <div class="form-item">
           <label for="price">가격</label>
-          <input type="text" v-model="hotelDetail.price" required />
+          <span v-if="!hotelState.isEditing">{{
+            hotelState.hotelDetail.price
+          }}</span>
+          <input
+            v-else
+            type="text"
+            v-model="hotelState.hotelDetail.price"
+            required
+          />
         </div>
         <div class="form-item">
           <label for="is_used">사용유무</label>
-          <select v-model="hotelDetail.is_used" required>
+          <span v-if="!hotelState.isEditing">{{
+            hotelState.hotelDetail.is_used
+          }}</span>
+          <select v-else v-model="hotelState.hotelDetail.is_used" required>
             <option value="Y">Yes</option>
             <option value="N">No</option>
           </select>
         </div>
       </div>
     </div>
-
-    <div class="button-container">
-      <button class="rounded-md btn-update" type="button" @click="saveHotel">
-        {{ newHotel ? "등록" : "저장" }}
-      </button>
-      <button class="rounded-md btn-close" type="button" @click="closeModal">
-        닫기
-      </button>
-    </div>
   </form>
 </template>
 
 <script>
-import { ref, inject, onMounted } from "vue";
-import { getHotel, getCountries, updateHotel } from "@/api/sales/HotelApi";
+import { inject } from "vue";
+import { updateHotel } from "@/api/sales/HotelApi";
 
 export default {
   name: "HotelDetail",
   setup(_, { emit }) {
-    const hotelCode = inject("hotelCode");
-    const hotelDetail = inject("hotelDetail");
-    const empId = inject("empId");
-    const countries = ref([]);
-
-    onMounted(async () => {
-      if (hotelCode && hotelCode.value) {
-        try {
-          const data = await getHotel({
-            hotelCode: hotelCode.value,
-            empId: empId,
-          });
-          hotelDetail.value = data;
-        } catch (error) {
-          console.error("Failed to fetch hotel details:", error);
-        }
-      }
-      try {
-        const countryData = await getCountries();
-        countries.value = countryData;
-      } catch (error) {
-        console.error("Failed to fetch countries:", error);
-      }
-    });
-
+    const hotelState = inject("hotelState");
     const submitForm = async () => {
       try {
         await updateHotel(hotelDetail.value);
@@ -96,10 +102,11 @@ export default {
     };
 
     return {
-      hotelDetail,
-      countries,
+      hotelState,
       submitForm,
     };
   },
 };
 </script>
+
+<style src="./HotelDashboard.css"></style>
