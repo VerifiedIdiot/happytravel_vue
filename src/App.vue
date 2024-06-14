d892f978b03c346aa5687aa3dd9437619b6570b0
jeffyun3061 <jeffyun3061@gmail.com>
2024-06-14 14:32:09 +0900

    login_fix

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,28 @@
 <template>
   <div id="app">
-    <Header />
+    <Header v-if="showHeader" />
     <router-view />
   </div>
 </template>
 
 <script>
+import { computed } from 'vue';
+import { useRoute } from 'vue-router';
 import Header from './components/header/Header.vue';
-
 
 export default {
   name: 'App',
   components: {
     Header,
-  }
+  },
+  setup() {
+    const route = useRoute();
+    const showHeader = computed(() => route.name !== 'Login');
+
+    return {
+      showHeader,
+    };
+  },
 }
 </script>
 
