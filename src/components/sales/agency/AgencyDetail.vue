0a1f030c9727b3eb96d653c430617da1fa5289f3
moonjiye <mjy6088@gmail.com>
2024-06-17 21:09:59 +0900

    24/06/17 Firebase 파일업로드/수정/삭제

--- src/components/sales/agency/AgencyDetail.vue
+++ src/components/sales/agency/AgencyDetail.vue
@@ -71,15 +71,55 @@
             required
           />
         </div>
+        <div class="form-item">
+          <label for="image">이미지</label>
+          <span v-if="!agencyState.isEditing">
+            <img
+              v-if="agencyState.agencyDetail.image_url"
+              :src="agencyState.agencyDetail.image_url"
+              alt="Agency Image"
+            />
+            <span v-else>이미지가 없습니다</span>
+          </span>
+          <div v-if="agencyState.isEditing" class="form-img">
+            <div class="form-img-pos">
+              <img
+                v-if="state.previewImageUrl || agencyState.agencyDetail.image_url"
+                :src="state.previewImageUrl || agencyState.agencyDetail.image_url"
+                alt="Agency Image"
+              />
+              <span v-else>이미지가 없습니다</span>
+              <button
+                v-if="agencyState.agencyDetail.image_url"
+                @click="deleteImageFile"
+                class="delete-btn"
+              >
+                X
+              </button>
+            </div>
+            <input type="file" @change="onFileChange" />
+          </div>
+        </div>
       </div>
     </div>
   </form>
 </template>
 
 <script setup>
-import { inject } from "vue";
+import { inject, reactive } from "vue";
+import {
+  uploadImage,
+  saveAgencyImageUrl,
+  deleteImage,
+  removeImageUrl,
+} from "../../../firebase";
 
 const agencyState = inject("agencyState");
+
+// 반응형 상태로 설정
+const state = reactive({
+  previewImageUrl: null,
+});
 
 const setCountryCode = () => {
   const data = agencyState.countries.find(
@@ -89,7 +129,54 @@ const setCountryCode = () => {
     agencyState.agencyDetail.country_code = data.country_code;
   }
 };
-</script>
 
-<style>
-</style>
+const onFileChange = async (event) => {
+  const file = event.target.files[0];
+  if (file) {
+
+    // 파일업로드시 미리보기
+    state.previewImageUrl = URL.createObjectURL(file);
+
+    try {
+      // URL
+      const imageUrl = await uploadImage(
+        file,
+        `images/${agencyState.agencyDetail.agency_code}`
+      );
+      agencyState.agencyDetail.imageUrl = imageUrl;
+
+      const storedImageUrl = imageUrl;
+
+      // Save
+      await saveAgencyImageUrl(
+        agencyState.agencyDetail.agency_code,
+        storedImageUrl
+      );
+    } catch (error) {
+      console.error("Error uploading image:", error);
+    }
+  }
+};
+
+const deleteImageFile = async () => {
+  const imageUrl = agencyState.agencyDetail.image_url;
+  const agencyCode = agencyState.agencyDetail.agency_code;
+
+  if (imageUrl && agencyCode) {
+    try {
+      // 이미지 숨기기
+      agencyState.agencyDetail.image_url = "";
+      state.previewImageUrl = null;
+
+      // Firebase에서 이미지 삭제
+      await deleteImage(imageUrl);
+      await removeImageUrl(agencyCode);
+    } catch (error) {
+      console.error("Error deleting image:", error);
+    }
+  } else {
+    console.warn("No image URL or agency code found");
+  }
+};
+</script>
+<style src="./AgencyDashboard.css"></style>
