f736ae6d960fa938856120ea36d79991b215628f
lastofyears21 <lastofyears21@gmail.com>
2024-06-18 22:30:39 +0900

    24/06/18 정벼리 승인 반려 버튼 추가

--- src/components/sales/package/PackageDashboard.vue
+++ src/components/sales/package/PackageDashboard.vue
@@ -5,18 +5,24 @@
         <div class="package-item">
           <div class="filters">
             <button
-              class="btn-filter btn-filter-waited"
-              @click="fetchPackages(assignState.waited)">
+              class="btn-filter"
+              :class="selectedFilter === 'waited' ? 'btn-selected' : 'btn-unselected'"
+              @click="selectFilter('waited')"
+            >
               진행중
             </button>
             <button
-              class="btn-filter btn-filter-assigned"
-              @click="fetchPackages(assignState.assigned)">
+              class="btn-filter"
+              :class="selectedFilter === 'assigned' ? 'btn-selected' : 'btn-unselected'"
+              @click="selectFilter('assigned')"
+            >
               완료
             </button>
             <button
-              class="btn-filter btn-filter-rejected"
-              @click="fetchPackages(assignState.rejected)">
+              class="btn-filter"
+              :class="selectedFilter === 'rejected' ? 'btn-selected' : 'btn-unselected'"
+              @click="selectFilter('rejected')"
+            >
               반려
             </button>
           </div>
@@ -27,38 +33,36 @@
           </div>
         </div>
       </div>
-      <table>
+      <table class="custom-table">
         <thead>
           <tr>
-            <th>여행상품 이름</th>
-            <th>여행국가</th>
-            <th>여행시작일</th>
-            <th>여행종료일</th>
-            <th>여행상품 판매 시작일</th>
-            <th>여행상품 판매 종료일</th>
-            <th>결제유무</th>
+            <th class="col-name">여행상품 이름</th>
+            <th class="col-country">여행국가</th>
+            <th class="col-start">여행시작일</th>
+            <th class="col-end">여행종료일</th>
+            <th class="col-sale-start">여행상품 판매 시작일</th>
+            <th class="col-sale-end">여행상품 판매 종료일</th>
+            <th class="col-payment">결제유무</th>
           </tr>
         </thead>
         <tbody>
-          <tr
-            v-for="pkg in packages"
-            :key="pkg.packageCode"
-            @click="openModal(pkg.packageCode)">
-            <td>{{ pkg.packageName }}</td>
-            <td>{{ pkg.country }}</td>
-            <td>{{ pkg.startDate }}</td>
-            <td>{{ pkg.endDate }}</td>
-            <td>{{ pkg.saleStartDate }}</td>
-            <td>{{ pkg.saleEndDate }}</td>
-            <td>
+          <tr v-for="pkg in packages" :key="pkg.packageCode" @click="openModal(pkg.packageCode)">
+            <td class="col-name">{{ pkg.packageName }}</td>
+            <td class="col-country">{{ pkg.country }}</td>
+            <td class="col-start">{{ pkg.startDate }}</td>
+            <td class="col-end">{{ pkg.endDate }}</td>
+            <td class="col-sale-start">{{ pkg.saleStartDate }}</td>
+            <td class="col-sale-end">{{ pkg.saleEndDate }}</td>
+            <td class="col-payment">
               <div class="status">
                 <span
                   class="status"
-                  v-bind:class="{
+                  :class="{
                     'status-waited': pkg.assignCode === '1000' && !packageState.isEditing,
                     'status-assigned': pkg.assignCode === '2000',
-                    'status-rejected': pkg.assignCode === '3000',
-                  }">
+                    'status-rejected': pkg.assignCode === '3000'
+                  }"
+                >
                   {{
                     pkg.assignCode === '1000'
                       ? '진행중'
@@ -83,7 +87,7 @@
 </template>
 
 <script setup>
-import { inject, onMounted } from 'vue';
+import { inject, onMounted, ref } from 'vue';
 import { getPackage, getCountries } from '@/api/sales/PackageApi';
 import PackageDetail from '@/components/sales/package/PackageDetail.vue';
 import PackageModal from '@/components/sales/package/PackageModal.vue';
@@ -98,9 +102,16 @@ const partnerState = inject('partnerState');
 const resetPackageState = inject('resetPackageState');
 const resetAllState = inject('resetAllState');
 
+const selectedFilter = ref('waited');
+
 onMounted(() => {
   fetchPackages();
 });
+
+const selectFilter = (filter) => {
+  selectedFilter.value = filter;
+  fetchPackages(assignState[filter]);
+};
 
 const openModal = async (pkgCode = '') => {
   packageState.packageCode = pkgCode;
@@ -113,7 +124,7 @@ const openModal = async (pkgCode = '') => {
           ...data,
           hotelCountry: data.country,
           flightCountry: data.country,
-          agencyCountry: data.country,
+          agencyCountry: data.country
         };
         partnerState.selectedCountryCode = data.countryCode;
       } else {
@@ -155,36 +166,35 @@ const closeModal = () => {
 @import 'tailwindcss/utilities';
 
 .wrapper {
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-  width: 100%;
-  height: auto;
+  @apply flex flex-col justify-center items-center w-full h-auto bg-gray-200;
 }
 
 .package-container {
   display: flex;
   flex-direction: column;
+  align-items: center;
   width: 95%;
   min-height: 85.5dvh;
+  @apply bg-gray-50 shadow-md;
 }
 
 .package-box {
   display: flex;
   flex-direction: column;
   justify-content: flex-end;
-  width: 100%;
+  width: 99%;
   height: 20dvh;
+  @apply bg-gray-50 shadow-sm;
 }
 
 .package-item {
   display: flex;
-  justify-content: flex-end;
+  justify-content: space-between;
   align-items: center;
   width: 100%;
   height: 70px;
 }
+
 .filters {
   display: flex;
   align-items: center;
@@ -192,69 +202,103 @@ const closeModal = () => {
   height: 100%;
 }
 
-.top-buttons {
-  display: flex;
-  justify-content: flex-end;
-  align-items: center;
-  width: 50%;
-  height: 100%;
-}
 .btn-filter {
-  @apply text-white font-bold rounded;
-  transition: background-color 0.3s ease;
+  @apply font-bold rounded transition ease-out;
   width: 100px;
-  height: 40px;
+  height: 36px;
   margin-right: 10px;
 }
 
-.btn-filter-waited {
-  @apply bg-orange-400;
+.btn-selected {
+  @apply bg-blue-700 text-white shadow-md;
+}
+
+.btn-unselected {
+  @apply bg-white text-black border shadow-sm;
 }
 
 .btn-filter-waited:hover {
   @apply bg-orange-700;
 }
 
-.btn-filter-assigned {
-  @apply bg-green-500;
-}
-
 .btn-filter-assigned:hover {
   @apply bg-green-700;
-}
-
-.btn-filter-rejected {
-  @apply bg-red-600;
 }
 
 .btn-filter-rejected:hover {
   @apply bg-red-900;
 }
 
-table {
-  border: 1px solid black;
-  border-spacing: 0;
+table.custom-table {
+  @apply border border-gray-200 border-spacing-0 bg-white rounded-b-lg;
+  width: 99%;
+}
+
+thead {
+  @apply bg-gray-200;
+  font-weight: bold;
+  border-collapse: collapse;
 }
 
 th,
 td {
-  vertical-align: middle;
-  overflow: hidden;
-  text-overflow: ellipsis;
-  white-space: nowrap;
-  .status {
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    width: 80%;
-  }
+  @apply align-middle overflow-hidden overflow-ellipsis whitespace-nowrap border-b border-gray-200 text-gray-700;
+  border-bottom: 1px solid rgba(0, 0, 0, 0.1);
+}
+
+th.col-name,
+td.col-name {
+  @apply w-1/6;
+}
+
+th.col-country,
+td.col-country,
+th.col-start,
+td.col-start,
+th.col-end,
+td.col-end,
+th.col-sale-start,
+td.col-sale-start,
+th.col-sale-end,
+td.col-sale-end {
+  @apply w-1/6;
+}
+
+th.col-payment,
+td.col-payment {
+  @apply w-1/12;
+}
+
+tbody tr:nth-child(even) {
+  @apply bg-gray-100;
+}
+
+tbody tr {
+  border-bottom: 1px solid rgba(0, 0, 0, 0.05); 
+}
+
+tbody tr td {
+  border-right: 1px solid rgba(0, 0, 0, 0.05); 
+}
+
+td {
+  cursor: pointer;
+}
+
+.status {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
 }
 
 @keyframes blink {
-  0%, 50% {
+  0%,
+  50% {
     display: none;
   }
-  51%, 100% {
+  51%,
+  100% {
     display: flex;
   }
 }
