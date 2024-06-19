ae6145a80358c6f13c542f8af23229ad07a47d3f
jeffyun3061 <jeffyun3061@gmail.com>
2024-06-19 17:24:15 +0900

    main fix

--- src/router/index.js
+++ src/router/index.js
@@ -1,9 +1,8 @@
 import { createRouter, createWebHashHistory } from "vue-router";
-// import store from '../store';
+import store from '../store';  
 
 // 컴포넌트 임포트
-import Login from "../components/auth/Login.vue"; // 네가 추가한 로그인 컴포넌트
-import Home from "../views/Home.vue";
+import LoginMain from "../views/login/LoginMain.vue";
 import HRMain from "../views/hr/HRMain.vue";
 import HREmp from "../views/hr/HREmp.vue";
 import SalesMain from "../views/sales/SalesMain.vue";
@@ -12,44 +11,36 @@ import SalesPackage from "../views/sales/SalesPackage.vue";
 import SalaryData from "@/views/salary/SalaryData.vue";
 import SalaryPayment from "@/views/salary/SalaryPayment.vue";
 import MyPage from "@/views/mypage/MyPage.vue";
-import MainPage from "../components/main/MainPage.vue"; // 네가 추가한 메인 컴포넌트
+import MainPage from "../views/main/MainPage.vue";
 
 const routes = [
   {
     path: "/",
     name: "Root",
-    // beforeEnter 가드 주석 처리
-    // beforeEnter: (to, from, next) => {
-    //   if (store.getters.isAuthenticated) {
-    //     next({ name: 'Home' });
-    //   } else {
-    //     next({ name: 'Login' });
-    //   }
-    // },
-    component: Home,
+    component: LoginMain,
   },
   {
     path: "/login",
     name: "Login",
-    component: Login,
+    component: LoginMain,
   },
   {
-    path: "/home",
-    name: "Home",
-    component: Home,
-    // meta: { requiresAuth: true }
+    path: "/main",
+    name: "MainPage",
+    component: MainPage,
+    meta: { requiresAuth: true },
   },
   {
     path: "/hr",
     name: "HRMain",
     component: HRMain,
-    // meta: { requiresAuth: true }
+    meta: { requiresAuth: true },
     children: [
       {
         path: "emp",
         name: "HREmp",
         component: HREmp,
-        // meta: { requiresAuth: true } 주석 처리
+        meta: { requiresAuth: true },
       },
     ],
   },
@@ -57,19 +48,19 @@ const routes = [
     path: "/sales",
     name: "SalesMain",
     component: SalesMain,
-    // meta: { requiresAuth: true }
+    meta: { requiresAuth: true },
     children: [
       {
         path: "partner",
         name: "SalesPartner",
         component: SalesPartner,
-        // meta: { requiresAuth: true }
+        meta: { requiresAuth: true },
       },
       {
         path: "package",
         name: "SalesPackage",
         component: SalesPackage,
-        // meta: { requiresAuth: true }
+        meta: { requiresAuth: true },
       },
     ],
   },
@@ -77,37 +68,37 @@ const routes = [
     path: "/salary/data",
     name: "SalaryData",
     component: SalaryData,
-    // meta: { requiresAuth: true }
+    meta: { requiresAuth: true },
   },
   {
     path: "/salary/payment",
     name: "SalaryPayment",
     component: SalaryPayment,
-    // meta: { requiresAuth: true }
+    meta: { requiresAuth: true },
   },
   {
     path: "/attendance",
     name: "AttendanceMain",
     component: () => import("../views/attendances/AttendanceMain.vue"),
-    // meta: { requiresAuth: true }
+    meta: { requiresAuth: true },
     children: [
       {
         path: "myAttendance",
         name: "MyAttendance",
         component: () => import("../views/attendances/MyAttendance.vue"),
-        // meta: { requiresAuth: true }
+        meta: { requiresAuth: true },
       },
       {
         path: "teamAttendance",
         name: "TeamAttendance",
         component: () => import("../views/attendances/TeamAttendance.vue"),
-        // meta: { requiresAuth: true }
+        meta: { requiresAuth: true },
       },
       {
         path: "adminAttendance",
         name: "AdminAttendance",
         component: () => import("../views/attendances/AdminAttendance.vue"),
-        // meta: { requiresAuth: true }
+        meta: { requiresAuth: true },
       },
     ],
   },
@@ -115,13 +106,7 @@ const routes = [
     path: "/mypage",
     name: "MyPage",
     component: MyPage,
-    // meta: { requiresAuth: true }
-  },
-  {
-    path: '/main',
-    name: 'MainPage',
-    component: MainPage,
-    // meta: { requiresAuth: true }
+    meta: { requiresAuth: true },
   },
 ];
 
@@ -130,13 +115,13 @@ const router = createRouter({
   routes,
 });
 
-// 전역 가드 주석 처리
-// router.beforeEach((to, from, next) => {
-//   if (to.meta.requiresAuth && !store.getters.isAuthenticated) {
-//     next({ name: 'Login' });
-//   } else {
-//     next();
-//   }
-// });
+// 전역 가드 추가
+router.beforeEach((to, from, next) => {
+  if (to.meta.requiresAuth && !store.getters.isAuthenticated) {
+    next({ name: 'Login' });
+  } else {
+    next();
+  }
+});
 
 export default router;
